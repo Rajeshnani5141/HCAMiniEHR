deca515851c3ff73d43abc1a91c5cc6d3d174868
rajesh <rajeshnani5141@gmail.com>
2026-01-12 17:43:30 +0530

    Fix .NET 8 compatibility - replace static assets methods

--- Program.cs
+++ Program.cs
@@ -28,14 +28,12 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
 
 app.Run();
